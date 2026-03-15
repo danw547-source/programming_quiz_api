22941e23efc0df393ae0fe494b29a307510cea64
Dan Wrigley <danw547@gmail.com>
2026-03-15 19:44:47 +0000

    fix: update production domain from .com to .net

--- app/main.py
+++ app/main.py
@@ -11,8 +11,8 @@ app.add_middleware(
 	allow_origins=[
 		"http://localhost:5173",
 		"http://127.0.0.1:5173",
-		"https://beatthebacklog.com",
-		"https://www.beatthebacklog.com",
+		"https://beatthebacklog.net",
+		"https://www.beatthebacklog.net",
 	],
 	allow_credentials=True,
 	allow_methods=["*"],
